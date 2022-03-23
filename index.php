6253ea23c507cc1110cb4f8aebb0b926d60f7935
Isaac Pereira do Prado <73193910+icpereiradoprado@users.noreply.github.com>
2022-03-23 19:21:03 -0300

    Update index.php
    Correção do atributo action

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Operações</title>
 </head>
 <body>
-    <form action="teste" method="post">
+    <form action="teste.php" method="post">
         <label for="valor1">1º Valor: </label>
         <input type="number" name="valor1" id="valor1" value=""><br><br>
         <label for="valor1">2º Valor: </label>
