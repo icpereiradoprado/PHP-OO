b25fca4a36a5bef8b22a09672b001db869a72c4e
alunos <alunos@etehort.ps.sp>
2022-03-23 22:26:03 -0300

    Exercício Operação - Finalizado

--- index.php
+++ index.php
@@ -5,14 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Operações</title>
+    <style>
+        body{
+            background-color: #EAEBED;
+        }
+        fieldset{
+            margin-top: 40px;
+            margin-bottom: 10px;
+            background-color: white;
+        }
+        .container{
+            display: flex;
+            flex-direction: column;
+            align-items: center; 
+        }
+        .opcoes{
+            margin-left: 80px;
+            width: 100px;
+            padding: 8px;  
+            border: none;
+            background-color: #006989;
+            color: white;
+            font-family: arial;
+        }
+        .opcoes:hover{
+            background-color: #01546d;
+        }
+    </style>
 </head>
 <body>
-    <form action="teste.php" method="post">
-        <label for="valor1">1º Valor: </label>
-        <input type="number" name="valor1" id="valor1" value=""><br><br>
-        <label for="valor1">2º Valor: </label>
-        <input type="number" name="valor2" id="valor2" value=""><br><br>
-        <input type="submit" value="btnEnviar" value="Enviar">
-    </form>
+    <div class="container">
+        <form action="teste.php" method="post">
+            <fieldset>
+                <legend>Valores:</legend>
+                <label for="valor1">1º Valor: </label>
+                <input type="number" name="valor1" id="valor1" value=""><br><br>
+                <label for="valor1">2º Valor: </label>
+                <input type="number" name="valor2" id="valor2" value=""><br><br>
+            </fieldset>
+            
+            <input type="submit" name="somar" class="opcoes" value="Somar"><br><br>
+            <input type="submit" name="subtrair" class="opcoes" value="Subtrair"><br><br>
+            <input type="submit" name="multiplicar" class="opcoes" value="Multiplicar"><br><br>
+            <input type="submit" name="dividir" class="opcoes" value="Dividir"><br><br>
+            <input type="submit" name="exponenciar" class="opcoes" value="Exponenciar"><br><br>
+        </form>
+    </div>
 </body>
 </html>
--- teste.php
+++ teste.php
@@ -1,23 +1,84 @@
 <?php
-    session_start();
     require_once 'Operacao.php';
 
     $op1 = new Operacao();
-    $op2 = new Operacao();
+    if(isset($_POST['somar'])){
+        $op1->setValor1($_POST['valor1']);
+        $op1->setValor2($_POST['valor2']);
+    }else{
+        $op1->setValor1($_POST['valor1']);
+        $op1->setValor2($_POST['valor2']);
+    }
 
-    $op1->setValor1(4);
-    $op1->setValor2(2);
-
-    echo "1º Valor: ";
-    echo $op1->getValor1();
-    echo "<br>";
-    echo "2º Valor: ";
-    echo $op1->getValor2();
-
-    echo "<br>Valor1 + Valor2 = ".$op1->somar();
-    echo "<br>Valor1 - Valor2 = ".$op1->subtrair();
-    echo "<br>Valor1 / Valor2 = ".$op1->dividir();
-    echo "<br>Valor1 * Valor2 = ".$op1->multiplicar();
-    echo "<br>Valor1 ^ Valor2 = ".$op1->exponenciar();
-    
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Operacões</title>
+    <style>
+        .container{
+            height: 773px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-color: #EAEBED;
+        }
+        .valores{
+            background-color: #694873;
+            width: 400px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .valor{
+            margin:0px;
+            color: white;
+        }
+        .resultados{
+            width: 400px;
+            background-color: #007090;
+            text-align: center;
+            color: white;
+        }
+        #back{
+            margin-top: 10px;
+            width: 100px;
+            padding: 8px;  
+            border: none;
+            background-color: #006989;
+            color: white;
+            font-family: arial;
+        }
+        #back:hover{
+            background-color: #01546d;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Resultados</h2>
+        <div class="resultados">
+            <?php
+                if(isset($_POST['somar'])){
+                    echo "<p class='resultado'>".$op1->getValor1()." + ".$op1->getValor2()." = ".$op1->somar()."</p>";
+                }elseif (isset($_POST['subtrair'])){
+                    echo "<p class='resultado'>".$op1->getValor1()." - ".$op1->getValor2()." = ".$op1->subtrair()."</p>";
+                }elseif(isset($_POST['dividir'])){
+                    echo "<p class='resultado'>".$op1->getValor1()." / ".$op1->getValor2()." = ".$op1->dividir()."</p>";
+                }elseif(isset($_POST['multiplicar'])){
+                    echo "<p class='resultado'>".$op1->getValor1()." x ".$op1->getValor2()." = ".$op1->multiplicar()."</p>";
+                }elseif(isset($_POST['exponenciar'])){
+                    echo "<p class='resultado'>".$op1->getValor1()." ^ ".$op1->getValor2()." = ".$op1->exponenciar()."</p>";
+                }
+            ?>
+        </div>
+
+        <a href="index.php"><button id="back">Voltar</button></a>
+    </div>
+</body>
+</html>
